2977180bb34700ee8a67d805622dd6db31e30e45
Keshav <aggarwalk721@gmail.com>
2021-11-18 13:07:45 +0530

    second commit

--- src/main/java/com/keshav/aws/hms/controller/HospitalController.java
+++ src/main/java/com/keshav/aws/hms/controller/HospitalController.java
@@ -11,16 +11,11 @@ import com.keshav.aws.hms.util.Hospital;
 public class HospitalController {
 
 	@GetMapping("/hospital")
-	public List<Hospital> getAllHospitals()
+	public String getAllHospitals()
 	{
 		
-		return dummyHospitalDB();
+		return "welcome to appolo hospitals";
 	}
 	
-	public List<Hospital> dummyHospitalDB()
-	{
-		
-		return List.of(new Hospital(1, "Appolo"),new Hospital(2, "HSBC"),new Hospital(3, "PGID"),new Hospital(4, "Martin Hospitals"));
-		
-	}
+	
 }
